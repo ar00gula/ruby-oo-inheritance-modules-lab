e389dafa8a8b4e99faca3421d754cca7f3387961
ar00gula <arianna.mayer@gmail.com>
2020-10-16 19:24:43 -0400

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,10 @@
+module Findable
+    
+    module ClassMethods
+        def find_by_name(name)
+            self.class_variable_get(:@@all).detect{|a| a.name == name}
+        end
+    end
+
+
+end
